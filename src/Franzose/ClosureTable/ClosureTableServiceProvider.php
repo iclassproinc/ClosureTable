786468c822548e543ae48fcdaed0ee7e9f446981
Jeremiah Fulbright <jeremiah@iclasspro.com>
2020-09-10 13:04:34 -0500

    port changes for laravel 6+

--- src/Franzose/ClosureTable/ClosureTableServiceProvider.php
+++ src/Franzose/ClosureTable/ClosureTableServiceProvider.php
@@ -2,7 +2,6 @@
 namespace Franzose\ClosureTable;
 
 use Illuminate\Support\ServiceProvider;
-use Franzose\ClosureTable\Console\ClosureTableCommand;
 use Franzose\ClosureTable\Console\MakeCommand;
 
 /**
@@ -12,19 +11,6 @@ use Franzose\ClosureTable\Console\MakeCommand;
  */
 class ClosureTableServiceProvider extends ServiceProvider
 {
-
-    /**
-     * Current library version
-     */
-    const VERSION = 4;
-
-    /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = false;
-
     /**
      * Bootstrap the application events.
      *
@@ -42,15 +28,11 @@ class ClosureTableServiceProvider extends ServiceProvider
     public function register()
     {
         // Here we register commands for artisan
-        $this->app->singleton('command.closuretable', function ($app) {
-            return new ClosureTableCommand;
-        });
-
         $this->app->singleton('command.closuretable.make', function ($app) {
             return $app['Franzose\ClosureTable\Console\MakeCommand'];
         });
 
-        $this->commands('command.closuretable', 'command.closuretable.make');
+        $this->commands('command.closuretable.make');
     }
 
     /**
